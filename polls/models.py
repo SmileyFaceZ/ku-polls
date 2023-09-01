8c239ab81ee36828ca1ea01f8c9ccc7a61f7c2f9
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-09-01 23:46:17 +0700

    Implement tutorial part 5

--- polls/models.py
+++ polls/models.py
@@ -10,10 +10,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self) -> bool:
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.question_text
 
 
@@ -22,5 +23,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.choice_text
